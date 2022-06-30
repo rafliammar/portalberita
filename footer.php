601bfc2af64f244613db8781fd502dbf2439a0bc
MuflihRahmatHidayat <84701002+MuflihRahmatHidayat@users.noreply.github.com>
2022-06-30 20:43:08 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+	</div>
+
+</div>
+
+<footer>
+	<div class="wrap">
+		<div class="pd10">
+			<div class="footer pd10">
+				Copyright &copy; 2019 | heriweb.com
+			</div>
+		</div>
+	</div>
+</footer>
+
+</body>
+</html>
