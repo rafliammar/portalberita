d3b3634e511d1cf74a486da441e77b900648f1d6
MuflihRahmatHidayat <84701002+MuflihRahmatHidayat@users.noreply.github.com>
2022-07-07 13:46:17 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+	</div>
+
+</div>
+
+<footer>
+	<div class="wrap">
+		<div class="pd10">
+			<div class="footer pd10">
+				Copyright &copy; 2022 | Kelompok1
+			</div>
+		</div>
+	</div>
+</footer>
+
+</body>
+</html>
